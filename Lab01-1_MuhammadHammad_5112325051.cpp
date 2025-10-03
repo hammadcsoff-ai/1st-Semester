9254bbccc692160cbca8b8a01b04c264b894ce2e
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-03 23:59:37 +0100

    Lab01-1_MuhammadHammad_5112325051.cpp

--- Lab01-1_MuhammadHammad_5112325051.cpp
+++ Lab01-1_MuhammadHammad_5112325051.cpp
@@ -1,4 +1,9 @@
-// This line prints "You are 10 years old."
-cout<<"You are 10 years old."<<endl
-// This line prints "You are too young to play the game."
-cout<<"You are too young to play the game."<<endl
+#include<iostream.h>
+using namespace std;
+
+int main{
+  // This line prints "You are 10 years old."
+  cout<<"You are 10 years old."<<endl;
+  // This line prints "You are too young to play the game."
+  cout<<"You are too young to play the game."<<endl;
+}
