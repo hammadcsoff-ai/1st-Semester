82bb6fef8f9f7c17ac7cf0d88879e17ca6b62dfb
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-03 23:57:45 +0100

    Lab01-2_MuhammadHammad_5112325051.cpp

--- Lab01-2_MuhammadHammad_5112325051.cpp
+++ Lab01-2_MuhammadHammad_5112325051.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main (){
+  // Storing my name, registration number and institute name
+  str name = "Muhammad Hammad";
+  str reg = "5112325051";
+  str ins = "Ibadat International University, Islamabad";
+
+  // Displaying one by one
+  cout<<name<<endl;
+  cout<<reg<<endl;
+  cout<<ins<<endl;
+  
+}
