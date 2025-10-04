173e565a696814563f52198d32f147502e8bb1ce
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-04 03:33:21 +0100

    Lab02-1_MuhammadHammad_5112325051.cpp

---
+++ Lab02-1_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  // Ask user for their age
+  cout<<"How old are you? ";
+  int x;
+  cin >> x;
+
+  // Print a message that includes their age
+  cout<<"Congratulations! You have survived "<<x<<" years in Pakistan."<<endl;
+
+  return 0;
+}
