31c806571144d4f325ae7e5d9380639a6e24ca06
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-08 23:39:40 +0100

    Lab03-5_MuhammadHammad_5112325051.cpp

---
+++ Lab03-5_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,27 @@
+// Write a program to print a formatted report of items with names, quantities, and prices
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Printing title
+    cout<<"Name\t\t\tQuantity\t\tPrice\n";
+    cout << "---------------------------------------\n";
+    
+    // Item 1
+    cout<<"Mangoes\t\t\t6\t\t\t\tRs.350\n";
+    
+    // Item 2
+    cout<<"Apples\t\t\t12\t\t\t\tRs.480\n";
+    
+    // Item 3
+    cout<<"Watermelon\t\t1\t\t\t\tRs.250\n";
+    
+    // Item 4
+    cout<<"Carrots\t\t\t18\t\t\t\tRs.220\n";
+    
+    // Item 5
+    cout<<"Bananas\t\t\t30\t\t\t\tRs.300\n";
+    
+    return 0;
+}
