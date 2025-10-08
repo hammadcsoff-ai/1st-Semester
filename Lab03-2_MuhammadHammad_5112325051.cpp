8f5d2497ea7f57cd3a30e41a3e05dc069c6d951d
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-08 19:30:29 +0100

    Lab03-2_MuhammadHammad_5112325051.cpp

---
+++ Lab03-2_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,26 @@
+// Write a program to find the largest between two numbers
+#include<iostream>
+using namespace std;
+
+int main () 
+{
+    // Declaring both variables
+    int x, y;
+    
+    // Taking numbers as input in those variables
+    cout<<"Enter the value of x: ";
+    cin >> x;
+    cout<<"Enter the value of y: ";
+    cin >> y;
+    
+    // Conditional statements to check which one is greater
+    if (x > y) {
+        cout<<"x is greater than y."<<endl;
+    }
+    else if (x < y){
+        cout<<"y is greater than x."<<endl;
+    }
+    else {
+        cout<<"Both numbers are equal."<<endl;
+    }
+}
