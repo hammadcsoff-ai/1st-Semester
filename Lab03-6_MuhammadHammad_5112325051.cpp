f52a9d3b01efe407f6b66d4c725ec48819dbb2f6
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-09 21:01:34 +0100

    Lab03-6_MuhammadHammad_5112325051.cpp

--- Lab03-6_MuhammadHammad_5112325051.cpp
+++ Lab03-6_MuhammadHammad_5112325051.cpp
@@ -1,4 +1,29 @@
 // Develop a program that takes user input for age as a string, converts it to an integer, and checks if the user is eligible to vote
 #include<iostream>
+#include<string>
 using namespace std;
 
+int main()
+{
+    // Declaring necessary variables
+    string Age;
+    int age;
+    
+    // Taking user's age as input
+    cout<<"How old are you? ";
+    cin >> Age;
+    
+    // Typecasting age into integer
+    age = stoi (Age);
+    
+    // Checking eligibility to vote
+    if (age >= 18){
+        cout<<"You are eligible to cast a vote."<<endl;
+    }
+    else{
+        cout<<"You are not eligible to cast a vote yet."<<endl;
+    }
+    
+    // Return 0 on successful execution
+    return 0;
+}
