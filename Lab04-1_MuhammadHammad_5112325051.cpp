7a24215dc91dfbac9a5525aa58043079aaa4b0c4
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-19 00:05:13 +0100

    Lab04-1_MuhammadHammad_5112325051.cpp

---
+++ Lab04-1_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,19 @@
+// Write a program that finds the highest number of 3 numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Declaring the necessary variables
+    int a, b, c;
+    
+    // Taking values as input
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"Enter another number: ";
+    cin>>b;
+    cout<<"Enter another number: ";
+    cin>>c;
+    
+    
+}
