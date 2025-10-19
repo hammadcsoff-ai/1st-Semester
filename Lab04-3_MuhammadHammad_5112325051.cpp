abf4eae798e7ea6158b31a5c7965ffb57bc1435d
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-19 02:02:08 +0100

    Lab04-3_MuhammadHammad_5112325051.cpp

---
+++ Lab04-3_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,33 @@
+// Write a program for a vending machine
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Items and their prizes
+    string items[5][2] = {{"Lays", "20"}, {"Gala", "15"}, {"Cola", "17"}, {"item4", "40"}, {"item5", "74"}};
+    int code;
+    // Available coins
+    int penny = 1;
+    int fifer = 5;
+    int tenner = 10;
+    int quarter = 25;
+    
+    // Displaying menu
+    cout<<"\tAvailable Items"<<endl;
+    cout<<"Code\tItem\t\tPrice"<<endl;
+    cout<<"__________________________"<<endl;
+    for (int i = 0; i < 5; i++){
+        cout<<i+1<<"\t\t"<<items[i][0]<<"\t\t"<<items[i][1]<<endl;
+    }
+    
+    // Prompt the user to select items
+    while(true){
+        cout<<"\nWhat would you like please?"<<endl;
+        cout<<"Enter the code for item or 0 to proceed to payment: ";
+        cin >> code;
+        if (code == 0){
+            break;
+        }
+    }
+}
