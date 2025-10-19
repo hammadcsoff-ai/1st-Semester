ff37a785d7ad551b366fb1a4c8804cd01204b42f
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-19 23:33:40 +0100

    Lab04-1_MuhammadHammad_5112325051.cpp

--- Lab04-1_MuhammadHammad_5112325051.cpp
+++ Lab04-1_MuhammadHammad_5112325051.cpp
@@ -5,15 +5,33 @@ using namespace std;
 int main()
 {
     // Declaring the necessary variables
-    int a, b, c;
+    int a, b, c, highest;
     
     // Taking values as input
-    cout<<"Enter a number: ";
-    cin>>a;
-    cout<<"Enter another number: ";
-    cin>>b;
-    cout<<"Enter another number: ";
-    cin>>c;
+    cout<<"Enter three numbers: "<<endl;
+    cin >> a >> b >> c;
     
+    // Finding highest using nested if-else
+    if (a >= c){
+        if (a >= b){
+            highest = a;
+        }
+        else{
+            highest = b;
+        }
+    }
+    else{
+        if (b >= c){
+            highest = b;
+        }
+        else{
+            highest = c;
+        }
+    }
     
+    // Display the highest number
+    cout<<endl;
+    cout<<highest<<" is the highest number."<<endl;
+    
+    return 0;
 }
