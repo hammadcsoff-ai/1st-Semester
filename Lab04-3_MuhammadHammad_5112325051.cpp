c6befc6929681771ec52cfce1291534cb69c73c1
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-10-20 00:00:27 +0100

    Lab04-3_MuhammadHammad_5112325051.cpp

--- Lab04-3_MuhammadHammad_5112325051.cpp
+++ Lab04-3_MuhammadHammad_5112325051.cpp
@@ -6,7 +6,9 @@ int main()
 {
     // Items and their prizes
     string items[5][2] = {{"Lays", "20"}, {"Gala", "15"}, {"Cola", "17"}, {"item4", "40"}, {"item5", "74"}};
+    string shopping[5];
     int code;
+    int item_count = 0;
     // Available coins
     int penny = 1;
     int fifer = 5;
@@ -23,11 +25,17 @@ int main()
     
     // Prompt the user to select items
     while(true){
-        cout<<"\nWhat would you like please?"<<endl;
+        cout<<"\nYour order: ";
+        for (string item : shopping){
+            cout<<item<<", ";
+        }
+        cout<<"\nWhat would you like to add please?"<<endl;
         cout<<"Enter the code for item or 0 to proceed to payment: ";
         cin >> code;
         if (code == 0){
             break;
         }
+        shopping[item_count] = items[code - 1][0];
+        item_count++;
     }
 }
