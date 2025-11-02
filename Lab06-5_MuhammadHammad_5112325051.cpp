a2411f7ccec3e845c3f39bbb7b5b69dc4a1b8fbc
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-11-02 02:34:16 +0000

    Lab06-5_MuhammadHammad_5112325051.cpp

--- Lab06-5_MuhammadHammad_5112325051.cpp
+++ Lab06-5_MuhammadHammad_5112325051.cpp
@@ -2,4 +2,34 @@
 #include<iostream>
 using namespace std;
 
-int main
+int main()
+{
+    // Declaring variables 
+    string students[5] = {"Hammad", "Ikram", "Salar", "Haris", "Hasnain"};
+    int theory[5];
+    int pract[5];
+    int total[5];
+    
+    // Taking marks as input
+    cout<<"Enter marks of students in theory and practical."<<endl;
+    for (int i = 0; i < 5; i++){
+        cout<<"Marks of "<<students[i]<<endl;
+        cout<<"Theory: ";
+        cin >> theory[i];
+        cout<<"Practical: ";
+        cin >> pract[i];
+        cout<<endl;
+    }
+    
+    // Adding the marks and storing them in a third array
+    for (int i = 0; i < 5; i++){
+        total[i] = theory[i] + pract[i];
+    }
+    
+    // Displaying total marks
+    for (int i = 0; i < 5; i++){
+        cout<<"Total marks of "<<students[i]<<" : "<<total[i]<<endl;
+    }
+    
+    return 0;
+}
