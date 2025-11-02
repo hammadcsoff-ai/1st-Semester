164711c732114660146927d4f04388f7aeaa8fb9
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-11-02 01:10:04 +0000

    Lab06-2_MuhammadHammad_5112325051.cpp

--- Lab06-2_MuhammadHammad_5112325051.cpp
+++ Lab06-2_MuhammadHammad_5112325051.cpp
@@ -3,3 +3,25 @@
 using namespace std;
 
 int main()
+{
+    // Declaring variables and taking inputs
+    int n;
+    cout<<"Enter number of subjects: ";
+    cin >> n;
+    int marks[n];
+    cout << "Enter marks of each subject;"<<endl;
+    for (int i = 0; i < n; i++){
+        cin >> marks[i];
+    }
+    
+    // Finding the sum
+    int sum = 0;
+    for (int i : marks){
+        sum += i;
+    }
+    
+    // Displaying the total marks
+    cout<<"Total marks are "<<sum<<endl;
+    
+    return 0;
+}
