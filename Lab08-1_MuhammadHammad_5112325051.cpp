bfb1b310a0530ac40dd6c2772c4bfbedc655bdea
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-12-03 21:06:57 +0000

    Lab08-1_MuhammadHammad_5112325051.cpp

---
+++ Lab08-1_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,12 @@
+// Create a one dimensional array of characters and store a string inside it by reading from standard input
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[15];
+    cout<<"Enter your name: ";
+    cin >> name;
+    
+    cout<<"Hello, "<<name<<endl;
+}
