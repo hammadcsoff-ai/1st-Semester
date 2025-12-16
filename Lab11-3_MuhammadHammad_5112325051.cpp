1d8d8c5cb8db20ab9345afc810adb6cade536181
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-12-16 02:12:25 +0000

    Lab11-3_MuhammadHammad_5112325051.cpp

--- Lab11-3_MuhammadHammad_5112325051.cpp
+++ Lab11-3_MuhammadHammad_5112325051.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
+    // Taking input
     string word;
     cout<<"Enter a word: ";
     cin >> word;
     
+    // Reverse and display
+    cout<<"Before reverse: "<<word<<endl;
+    reverse(word.begin(), word.end());
+    cout<<"After reverse: "<<word<<endl;
     
+    return 0;
 }
