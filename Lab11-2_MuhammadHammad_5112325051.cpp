c935d3037c69e20e8f697f600d1275ff5fd60c29
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-12-16 01:36:48 +0000

    Lab11-2_MuhammadHammad_5112325051.cpp

---
+++ Lab11-2_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    // Taking input
+    float n;
+    cout<<"Enter a number to find the square root: ";
+    cin >> n;
+    
+    // Finding and displaying square root
+    cout<<"The square root is "<<sqrt(n)<<endl;
+    
+    return 0;
+}
