e0351cefbddbdc224e41b11d0ab9b5f0332b9a37
hammadcsoff-ai <hammadcsoff@gmail.com>
2025-12-19 00:00:52 +0000

    Lab12-1_MuhammadHammad_5112325051.cpp

---
+++ Lab12-1_MuhammadHammad_5112325051.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// Function prototypes
+int volume(int l);
+float volume(int r, int h);
+
+int main()
+{
+    // Taking inputs
+    int l, r, h;
+    cout<<"Length of cube: ";
+    cin >> l;
+    cout<<"Radius of cylinder: ";
+    cin >> r;
+    cout<<"Height of cylinder: ";
+    cin >> h;
+    
+    // Calculating and displaying
+    cout<<"The volume of cube is "<<volume(l)<<endl;
+    cout<<"The volume of cylinder is "<<volume(r, h)<<endl;
+    
+    return 0;
+}
+
+// Function for volume of cube
+int volume(int l){
+    return l*l*l;
+}
+
+// Function for volume of cylinder
+float volume(int r, int h){
+    float pie = 22.0/7;
+    cout<<pie<<endl;
+    return pie*r*r*h;
+}
